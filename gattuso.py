1c2d359b5d9d1dc76c28b070ece12bc1fc319989
Siddharth Jha <siddharthjha@outlook.com>
2019-10-22 01:06:56 +0530

    First commit with code building, now to add classes and other stuff

---
+++ gattuso.py
@@ -0,0 +1,144 @@
+# /usr/bin/env python
+import yaml
+import os
+
+class Params:
+	def __init__(self):
+		self.package_name = "my_pkg"
+		self.class_name = "MyPkg"
+		self.lib_name = "MyPkg"
+		self.pkg_version = "0.1.1"
+		self.pkg_description = "My Package"
+		self.maintainer_email = "siddharthjha@outlook.com"
+		self.maintainer_name = "Siddharth Jha"
+		self.ros = True
+		self.opencv = True
+		self.gtsam = True
+		self.ceres = True
+		self.pcl = True
+		self.ros_pkgs = ["roscpp", "rospy", "std_msgs", "geometry_msgs"]
+		self.cmake_minimum_req = "2.8.3"
+		self.cpp_version = "11"
+		self.year = "2019"
+		self.package_name_full = "My Package"
+
+def gen_cmakelists(params):
+	f = open("CMakeLists.txt","w+")
+	f.write("cmake_minimum_required(VERSION " + params.cmake_minimum_req + ")\n")
+	f.write("project(" + params.package_name + ")\n\n")
+	f.write("set(CMAKE_CXX_STANDARD " + params.cpp_version + ")\n\n")
+	if params.ros:
+		f.write("find_package(catkin REQUIRED COMPONENTS")
+		for i in params.ros_pkgs:
+			f.write(" "+i)
+		f.write(")\n\n")
+
+	f.write("include_directories(\n")
+	f.write("\t\tinclude/\n")
+	if params.ros:
+		f.write("\t\t${catkin_INCLUDE_DIRS}\n")
+	f.write(")\n\n")
+
+	if params.ros:
+		f.write("catkin_package()\n")
+	f.write("add_executable(")
+	f.write(params.package_name + "_node src/" + params.package_name + "_node.cc src/" + params.class_name + ".cc")
+	f.write(")\n")
+	f.write("target_link_libraries(" + params.package_name + "_node ")
+	if params.ros:
+		f.write("${catkin_LIBRARIES}")
+	f.write(")\n")
+
+def add_license(params, f):
+	license_txt = "/*\n\t" + params.package_name_full + "\n\tCopyright (C) " +  params.year +  " " + params.maintainer_name + """\n\tThis program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.\n*/\n"""
+	f.write(license_txt)
+
+def gen_package_xml(params):
+	f = open("package.xml","w+")
+	f.write('<?xml version="1.0"?>\n<package>\n')
+	f.write("\t<name>" + params.package_name + "</name>\n")
+	f.write("\t<version>" + params.pkg_version + "</version>\n")
+	f.write("\t<description>" + params.pkg_description + "</description>\n\n")
+	f.write('"\t<maintainer email="' + params.maintainer_email + '">' + params.maintainer_name + "</maintainer>\n\n")
+	f.write("\t<license>BSD</license>\n\n")
+	f.write("\t<buildtool_depend>catkin</buildtool_depend>\n")
+	for i in params.ros_pkgs:
+		f.write("\t<build_depend>" + i + "</build_depend>\n")
+	for i in params.ros_pkgs:
+		f.write("\t<run_depend>" + i + "</run_depend>\n")
+	f.write("\n\t<export>\n")
+	f.write("\t</export>\n\n")
+	f.write("</package>")
+
+
+def gen_node(params, path_orig):
+	pathsrc = os.path.join(path_orig, "src")
+	pathincprev = os.path.join(path_orig, "include")
+	pathinc = os.path.join(pathincprev, params.package_name)
+	os.chdir(pathsrc)
+	s = open(params.package_name + "_node.cc","w+")
+	add_license(params, s)
+	s.write("#include <" + params.package_name + "/" + params.package_name + "_node.h>\n\n")
+	s.write("int main(int argc, char** argv) {\n")
+	s.write("}")
+	os.chdir(pathinc)
+	i = open(params.package_name + "_node.h","w+")
+	add_license(params, i)
+	i.write("#pragma once")
+
+def gen_cpp(params, path_orig):
+	pathsrc = os.path.join(path_orig, "src")
+	pathincprev = os.path.join(path_orig, "include")
+	pathinc = os.path.join(pathincprev, params.package_name)
+	os.chdir(pathsrc)
+	s = open(params.class_name + ".cc","w+")
+	add_license(params, s)
+	s.write("#include <" + params.package_name + "/" + params.class_name + ".h>\n\n")
+	os.chdir(pathinc)
+	i = open(params.class_name + ".h","w+")
+	add_license(params, i)
+	i.write("#pragma once")
+
+def read_params():
+	params_ = Params()
+	return params_
+
+
+if __name__ == "__main__":
+	params = read_params()
+	path = os.path.join(os.getcwd(), params.package_name)
+	path_orig = path
+	if not os.path.exists(path):
+		os.mkdir(path)
+	os.chdir(path)
+	path = os.path.join(os.getcwd(), "src")
+	if not os.path.exists(path):
+		os.mkdir(path)
+	path = os.path.join(os.getcwd(), "include")
+	if not os.path.exists(path):
+		os.mkdir(path)
+		os.chdir(path)
+		path = os.path.join(os.getcwd(), params.package_name)
+		if not os.path.exists(path):
+			os.mkdir(path)
+	os.chdir(path_orig)
+	gen_cmakelists(params)
+	if params.ros:
+		gen_package_xml(params)
+	gen_node(params, path_orig)
+	gen_cpp(params, path_orig)
+
+
+
