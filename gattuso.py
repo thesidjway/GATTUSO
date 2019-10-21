c7c64fa58ab73c8176feff5a2a7d11ffdcb3d4f5
Siddharth Jha <siddharthjha@outlook.com>
2019-10-22 02:23:20 +0530

    YAML read complete, classes added, common libs included

--- gattuso.py
+++ gattuso.py
@@ -1,6 +1,7 @@
 # /usr/bin/env python
 import yaml
 import os
+import sys
 
 class Params:
 	def __init__(self):
@@ -16,11 +17,13 @@ class Params:
 		self.gtsam = True
 		self.ceres = True
 		self.pcl = True
+		self.eigen = True
 		self.ros_pkgs = ["roscpp", "rospy", "std_msgs", "geometry_msgs"]
 		self.cmake_minimum_req = "2.8.3"
 		self.cpp_version = "11"
 		self.year = "2019"
 		self.package_name_full = "My Package"
+		self.node_object = "mpn"
 
 def gen_cmakelists(params):
 	f = open("CMakeLists.txt","w+")
@@ -33,21 +36,57 @@ def gen_cmakelists(params):
 			f.write(" "+i)
 		f.write(")\n\n")
 
-	f.write("include_directories(\n")
+	if params.eigen:
+		f.write("find_package (Eigen3 REQUIRED)\n")
+	if params.opencv:
+		f.write("find_package (OpenCV REQUIRED)\n")
+	if params.pcl:
+		f.write("find_package (PCL REQUIRED COMPONENTS common io) \n")
+	if params.ceres:
+		f.write("find_package (Ceres REQUIRED) \n")
+	if params.gtsam:
+		f.write("find_package (GTSAM REQUIRED)\n")
+
+	f.write("\ninclude_directories(\n")
 	f.write("\t\tinclude/\n")
+	if params.eigen:
+		f.write("\t\tEigen\n")
 	if params.ros:
 		f.write("\t\t${catkin_INCLUDE_DIRS}\n")
-	f.write(")\n\n")
+	if params.opencv:
+		f.write("\t\t${OpenCV_INCLUDE_DIRS}\n")
+	f.write(")\n")
 
 	if params.ros:
-		f.write("catkin_package()\n")
+		f.write("\ncatkin_package(\n")
+		f.write("\tINCLUDE_DIRS include\n")
+		f.write("\tLIBRARIES " + params.package_name + "\n")
+		f.write("\tCATKIN DEPENDS")
+		for i in params.ros_pkgs:
+			f.write(" " + i)
+		f.write("\n)\n\n")
 	f.write("add_executable(")
 	f.write(params.package_name + "_node src/" + params.package_name + "_node.cc src/" + params.class_name + ".cc")
 	f.write(")\n")
-	f.write("target_link_libraries(" + params.package_name + "_node ")
+	f.write("target_link_libraries(" + params.package_name + "_node")
 	if params.ros:
-		f.write("${catkin_LIBRARIES}")
+		f.write(" ${catkin_LIBRARIES}")
+	if params.ceres:
+		f.write(" ceres")
+	if params.opencv:
+		f.write(" ${OpenCV_LIBRARIES}")
+	if params.gtsam:
+		f.write(" gtsam gtsam_unstable")
+	if params.pcl:
+		f.write(" ${PCL_COMMON_LIBRARIES} ${PCL_IO_LIBRARIES}")
 	f.write(")\n")
+
+def constdest(params, name, f):
+	f.write(name + "::" + name + "() {\n")
+	f.write("}\n\n")
+	f.write(name + "::~" + name + "() {\n")
+	f.write("}\n\n")
+
 
 def add_license(params, f):
 	license_txt = "/*\n\t" + params.package_name_full + "\n\tCopyright (C) " +  params.year +  " " + params.maintainer_name + """\n\tThis program is free software; you can redistribute it and/or
@@ -82,6 +121,13 @@ def gen_package_xml(params):
 	f.write("\t</export>\n\n")
 	f.write("</package>")
 
+def intmain(params, s):
+	s.write("int main(int argc, char** argv) {\n")
+	s.write("\t" + params.package_name + "_node " + params.node_object + ";\n")
+	if params.ros:
+		s.write("\tros::init(argc, argv, " + '"params.package_name"' + ");\n")
+		s.write("\t" + params.node_object + ".initCallbacks();\n")
+	s.write("}")
 
 def gen_node(params, path_orig):
 	pathsrc = os.path.join(path_orig, "src")
@@ -91,12 +137,27 @@ def gen_node(params, path_orig):
 	s = open(params.package_name + "_node.cc","w+")
 	add_license(params, s)
 	s.write("#include <" + params.package_name + "/" + params.package_name + "_node.h>\n\n")
-	s.write("int main(int argc, char** argv) {\n")
-	s.write("}")
+	constdest(params, params.package_name + "_node", s)
+	s.write("void " + params.package_name + "_node::initCallbacks() {\n")
+	s.write("}\n\n")
+	intmain(params, s)
 	os.chdir(pathinc)
 	i = open(params.package_name + "_node.h","w+")
 	add_license(params, i)
-	i.write("#pragma once")
+	i.write("#pragma once\n\n")
+	if params.ros:
+		i.write("#include <ros/ros.h>\n")
+	i.write("#include <" + params.package_name + "/" + params.class_name + ".h>\n\n")
+	i.write("class " + params.package_name + "_node {\n")
+	i.write("private:\n")
+	if params.ros:
+		i.write("\tros::NodeHandle nh_;\n")
+	i.write("public:\n")
+	i.write("\t"+params.package_name+"_node();\n")
+	i.write("\t~"+params.package_name+"_node();\n")
+	if params.ros:
+		i.write("\tvoid initCallbacks();\n")
+	i.write("};\n")
 
 def gen_cpp(params, path_orig):
 	pathsrc = os.path.join(path_orig, "src")
@@ -106,18 +167,48 @@ def gen_cpp(params, path_orig):
 	s = open(params.class_name + ".cc","w+")
 	add_license(params, s)
 	s.write("#include <" + params.package_name + "/" + params.class_name + ".h>\n\n")
+	constdest(params, params.class_name, s)
 	os.chdir(pathinc)
 	i = open(params.class_name + ".h","w+")
 	add_license(params, i)
-	i.write("#pragma once")
+	i.write("#pragma once\n\n")
+	i.write("class " + params.class_name + " {\n")
+	i.write("private:\n")
+	i.write("public:\n")
+	i.write("\t"+params.class_name+"();\n")
+	i.write("\t~"+params.class_name+"();\n")
+	i.write("};\n")
 
-def read_params():
+def read_params(config_file):
 	params_ = Params()
+	fil = yaml.load(open(config_file))
+	params_.package_name = fil['package_name']
+	params_.class_name = fil['class_name']
+	params_.lib_name = fil['lib_name']
+	params_.pkg_version = fil['pkg_version']
+	params_.pkg_description = fil['pkg_description']
+	params_.maintainer_email = fil['maintainer_email']
+	params_.maintainer_name = fil['maintainer_name']
+	params_.ros = fil['ros']
+	params_.opencv = fil['opencv']
+	params_.gtsam = fil['gtsam']
+	params_.ceres = fil['ceres']
+	params_.pcl = fil['pcl']
+	params_.eigen = fil['eigen']
+	params_.ros_pkgs = fil['ros_pkgs']
+	params_.cmake_minimum_req = fil['cmake_minimum_req']
+	params_.cpp_version = fil['cpp_version']
+	params_.year = fil['year']
+	params_.package_name_full = fil['package_name_full']
+	params_.node_object = fil['node_object']
 	return params_
 
 
 if __name__ == "__main__":
-	params = read_params()
+	if len(sys.argv) is not 2:
+		print "Invalid syntax. Usage: python gattuso.py config.py"
+		exit()
+	params = read_params(sys.argv[1])
 	path = os.path.join(os.getcwd(), params.package_name)
 	path_orig = path
 	if not os.path.exists(path):
@@ -139,6 +230,3 @@ if __name__ == "__main__":
 		gen_package_xml(params)
 	gen_node(params, path_orig)
 	gen_cpp(params, path_orig)
-
-
-
